33bf6a4f733b39aba104ffef2145889e5b13b849
Kareem643 <nyumba29@gmail.com>
2022-10-16 01:45:02 +0300

    update in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+
+/**
+* main - C program that prints a sentence
+* Return: 1 is returned
+*/
+
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  59);
-
-		return (1);
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
